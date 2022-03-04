24762d86e358570995159dbc638f99190cc36eea
gandalf3 <gandalf3@blendermonkey.com>
2022-03-03 23:09:38 -0800

    Add tests for chooser

---
+++ chooser.py
@@ -0,0 +1,11 @@
+
+def parse_choices(choices_str: str) -> list:
+    # Handle the following edge cases
+
+    # or       -> [or]
+    # or or or -> [or, or]
+    # ? or ?   -> [?,  ?]
+    # ? ? ?    -> [? ? ?]
+
+
+    return []
