91b385706452b111dc5c6e7be6733351a7e02b9a
gandalf3 <gandalf3@blendermonkey.com>
2022-03-04 05:00:52 -0800

    Partly working chooser parser thing
    Too tired to properly explain this but I want you to have it

--- tests/test_chooser.py
+++ tests/test_chooser.py
@@ -5,40 +5,90 @@ from distracticat import chooser
 
 
 def test_simple():
-    choices = chooser.parse_choices("this or that?")
-    assert choices is ["this", "that"]
+    choices, feedback = chooser.parse_choices("this or that?")
+    assert choices == ["this", "that"]
+    assert feedback is None
 
 def test_surrounding_whitespace():
-    choices = chooser.parse_choices("  this  or that ?   ? ")
-    assert choices is ["this", "that"]
+    choices, feedback = chooser.parse_choices("  this  or that ?   ? ")
+    assert choices == ["this", "that"]
+    assert feedback is None
 
 def test_single_or():
-    choices = chooser.parse_choices("or")
-    assert choices is ["or"]
+    choices, feedback = chooser.parse_choices("or")
+    assert choices is None
+    assert feedback is not None
 
 def test_two_or():
-    choices = chooser.parse_choices("or or")
-    assert choices is ["or", ""]
+    choices, feedback = chooser.parse_choices("or or")
+    assert choices is None
+    assert feedback is not None
 
 def test_three_or():
-    choices = chooser.parse_choices("or or or")
-    assert choices is ["or", "or"]
+    choices, feedback = chooser.parse_choices("or or or")
+    assert choices == ["or"]
+    assert feedback is not None
 
 def test_four_or():
-    choices = chooser.parse_choices("or or or or")
-    assert choices is ["or", "or", ""]
+    choices, feedback = chooser.parse_choices("or or or or")
+    assert choices == ["or", "or", ""]
+    assert feedback is not None
 
 def test_five_or():
-    choices = chooser.parse_choices("or or or or or")
-    assert choices is ["or", "or", "or"]
+    choices, feedback = chooser.parse_choices("or or or or or")
+    assert choices == ["or"]
+    assert feedback is not None
+
+def test_weird_or():
+    choices, feedback = chooser.parse_choices("or thing or")
+    assert choices == ["thing", "or"]
+
+def test_weird_or2():
+    choices, feedback = chooser.parse_choices("or or thing or")
+    assert choices is ["thing", ""]
+
+def test_weird_or3():
+    choices, feedback = chooser.parse_choices("or or or thing or")
+    assert choices == ["thing", ""]
+
+def test_weird_or4():
+    choices, feedback = chooser.parse_choices("or thing or or or")
+    assert choices == ["thing", ""]
 
 def test_only_question():
-    choices = chooser.parse_choices("? or ?")
-    assert choices is ["?", "?"]
+    choices, feedback = chooser.parse_choices("? or ?")
+    assert choices == ["?", "?"]
 
 def test_leading_question():
-    choices = chooser.parse_choices("?ok??? or ?")
-    assert choices is ["?ok???", "?"]
+    choices, feedback = chooser.parse_choices("?ok??? or ?")
+    assert choices == ["?ok???", "?"]
+    assert feedback is None
+
+def test_spaced_question():
+    choices, feedback = chooser.parse_choices("ok? ?")
+    assert choices == ["ok"]
+    assert feedback is None
+
+def test_internal_question():
+    choices, feedback = chooser.parse_choices("o?k? ?")
+    assert choices == ["o?k? ?"]
+    assert feedback is None
+
+def test_multi_question():
+    choices, feedback = chooser.parse_choices("??? or ??")
+    assert choices == ["???", "??"]
+    assert feedback is None
+
+def test_single_question():
+    choices, feedback = chooser.parse_choices("?")
+    assert choices == ["?"]
+    assert feedback is None
+
+def test_internal_space():
+    choices, feedback = chooser.parse_choices("meow meow? or pie")
+    assert choices == ["meow meow", "pie"]
+    assert feedback is None
+
 
 
 if __name__ == '__main__':
